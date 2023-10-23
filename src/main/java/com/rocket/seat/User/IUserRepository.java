9539840a4e0b519ec7c476a4d28ce3e8cb982818
darknessstonight <dudisfonseca.16@gmail.com>
2023-10-23 14:40:48 -0300

    Initial commit

---
+++ src/main/java/com/rocket/seat/User/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.rocket.seat.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+
+}
